d9603a93e95ec5ae02702326ddde2e8e909186ab
Nicolás Valor <nicoo.valor@gmail.com>
2023-04-24 16:24:38 -0300

    add(NavigationBar):basic html

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,5 +1,16 @@
 const NavitationBar = (): JSX.Element => {
-    return <></>
+    return <>
+        <div className="nav">
+            <div className="logo">
+                <h2 className="nav-logo">Nico</h2>
+            </div>
+            <div className="buttons">
+                <div className="nav-bttn">Work</div>
+                <div className="nav-bttn">About</div>
+                <div className="nav-bttn">Contact</div>
+            </div>
+        </div>
+    </>
 }
 
 export default NavitationBar
